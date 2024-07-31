9b1a38b15f30cc479df027fe34f24cdd4ca1289f
krishna0421 <krishna.kota08@gmail.com>
2024-07-31 05:19:55 +0000

    [LeetCode Sync] Runtime - 1 ms (88.15%), Memory - 42.6 MB (10.99%)

---
+++ leet_solves_java/1528-kids-with-the-greatest-number-of-candies/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+   public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    List<Boolean> res = new ArrayList<Boolean>();
+    int a = 0;
+    int max=maxValue(candies);
+        for (int i = 0; i <= candies.length-1; i++){
+             a = candies[i] + extraCandies;
+             if(a>=max) res.add(true);
+             else res.add(false);
+        }
+        return res;
+    }  
+    
+    public static int maxValue(int[] numbers){
+        int max=0;
+        for(int i=0; i < numbers.length; i++){
+            if(max<numbers[i]) {
+                max=numbers[i];
+            }
+        }
+        
+        return max;
+        
+    }
+}
